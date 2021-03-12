13233998f3deea2f56b557f3a4104eda952cd405
Ilsur <harpegnathos.venator@gmail.com>
2021-03-12 15:34:37 +0300

    Initial commit

---
+++ src/main/java/Send.java
@@ -0,0 +1,33 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) {
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+
+
+        try (Connection connection = connectionFactory.newConnection()) {
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+            String message = "Hello Wolrd!";
+
+            channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
